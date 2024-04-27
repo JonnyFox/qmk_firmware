ff944d0c0f3ba8f68e1498aad719611e226466f6
Jonny Fox <fox.jonny@gmail.com>
2024-04-27 14:57:50 +0200

    - Enabling and disabling pad mode

--- keyboards/sofle_choc/sofle_choc.c
+++ keyboards/sofle_choc/sofle_choc.c
@@ -92,7 +92,9 @@ void print_status_narrow(void) {
     }
     oled_write_P(PSTR("\n\n"), false);
     led_t led_usb_state = host_keyboard_led_state();
-    oled_write_ln_P(PSTR("CPSLK"), led_usb_state.caps_lock);
+    if (led_usb_state.caps_lock) {
+        oled_write_P(PSTR("CAPS"), false);
+    }
 }
 
 bool oled_task_kb(void) {
